8185b3c871336e2b00cfb489f02cb95c26322c8a
Marijan <marijanv@yahoo.com>
2017-08-15 09:46:49 -0700

    Flicker search -- Swift 3
    Simple Flicker search - Swift 3

---
+++ FlickrSearch/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch
+//
+//  Created by Marijan Vukcevich on 2/21/17.
+//  Copyright © 2017 Marijan Vukcevich. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
